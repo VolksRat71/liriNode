8316dd3a12bc6a746f97f5a7951e94ef837238fc
VolksRat71 <nathanieljryan1994@gmail.com>
2019-06-13 18:38:05 -0700

    further build with specific functions to cases

--- liri.js
+++ liri.js
@@ -1,32 +1,57 @@
+//application first request's for .gitignored'd .env folder
 require("dotenv").config();
 
+//application required packages before running
+var fs = require("fs");
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 
 var spotify = new Spotify(keys.spotify);
 
+// capture GitBash command line values
 var userCommand = process.argv[2];
 var userInput = process.argv.slice(3).join(" ");
-console.log(userInput);
 
-function userValue (command) {
-    switch (command){
+// command exicution controller
+function userValue () {
+    switch (userCommand){
         case `concert-this`:
-            console.log("concert called")
+            concertDisplay();
             break;
         
         case `spotify-this-song`:
-            console.log("spotify called")
+            songDisplay();
             break;
         
         case `movie-this`:
-            console.log("movie called")
+            movieDisplay();
             break;
         
         case `do-what-it-says`:
-            console.log("says called")
+            failSafeDisplay()
             break;
     }
 }
 
-userValue(userCommand);
+userValue();
+
+// case exicution functions
+function concertDisplay() {
+    console.log("Concert called " + userInput)
+};
+
+function songDisplay() {
+    console.log("Spotify called " + userInput)
+};
+
+function movieDisplay() {
+    console.log("Movie called " + userInput)
+};
+
+function failSafeDisplay() {
+fs.readFile("random.txt", 'utf8', function(err, data) {
+  if (err) throw err;
+  console.log(data)
+  userValue(data);
+});
+};
